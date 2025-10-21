df382caf205a1337bae792b70d44888b0d054d0f
JOSnow985 <jaden.olvera@students.snow.edu>
2025-10-21 11:40:33 -0600

    Checkpoint 2: Greeting and Description

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
 ﻿// Jaden Olvera, 10-21-25, Lab 7 - Pig Latin / Encoder
+Console.Clear();
+Console.WriteLine("Welcome to a little encoding program!");
+Console.WriteLine("We'll take a phrase from you and encode it with an offset, and into Pig Latin, just for fun!");
--- obj/Debug/net8.0/lab-7-pig-latin-JOSnow985.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-7-pig-latin-JOSnow985.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-7-pig-latin-JOSnow985")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b3cf5ce35aa602a3106cb71b7f42e04a23c87b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56bef6d874b6192ed8ad85bd932354c3d23697ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-7-pig-latin-JOSnow985")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-7-pig-latin-JOSnow985")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
